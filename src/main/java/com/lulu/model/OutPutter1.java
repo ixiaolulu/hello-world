dac0d30451389b94651d05237e964cc6ad39f768
xianlu.ding <xianlu.ding@guanaitong.com>
2017-10-27 14:45:51 +0800

    lock锁学习

---
+++ src/main/java/com/lulu/model/OutPutter1.java
@@ -0,0 +1,36 @@
+package com.lulu.model;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * Created by Administrator on 2017/10/27.
+ */
+public class OutPutter1 {
+    //锁对象
+    private  ReentrantLock  lock = new ReentrantLock();
+
+    public void output(String name) {
+        //获取锁
+        lock.lock();
+        System.out.println(Thread.currentThread()+":"+name+":加锁:"+lock.toString());
+        System.out.println(lock.getHoldCount()+":"+lock.isLocked());
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        try {
+            for (int i = 0; i < name.length(); i++) {
+                System.out.println(name.charAt(i));
+            }
+        } finally {
+            //释放锁
+            lock.unlock();
+            System.out.println(Thread.currentThread()+":"+name+":释放锁:"+lock.toString());
+            System.out.println(lock.getHoldCount()+":"+lock.isLocked());
+        }
+    }
+
+
+}
